c72ac07c2ae43539903355af7856c92bac3f92c9
MatheusBryan31 <BryanAlmeida56@hotmail.com>
2026-09-16 10:54:43 -0300

    Corrigindo erros: Coloquei as classes utilitárias na tag a, que é um elemento inline. Realoquei as classes utilitárias colocando-as na tag div - que um elemento do estilo em blocos. Também, comecei a lógica da interação do botão visualizar senha. Ainda estou codificando(matutando), mas, amanhã, eu termino essa função. Deixei o comentário nela para vocês verem que é ela quem eu estou modificando.

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+/* Função para fazer os olhos da senha interagirem */
+document.addEventListener("DOMContentLoaded", function(){
+    const botaoMostrarSenha = document.getElementById("mostrarSenha");
+    const campoSenha = document.getElementById("senha");
+    const iconeSenha = document.getElementById("iconeSenha");
+
+    addEventListener("click", function(){
+        console.log("Clicou no botão!" /* Isso é um teste, repaziada. */);
+    });
+}); // Para garantir que o HTML exista antes do JS rodar.
