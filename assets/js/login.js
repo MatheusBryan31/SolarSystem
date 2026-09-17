2c9429432c51ed395c1c6bad68eeb905062f59d7
MatheusBryan31 <BryanAlmeida56@hotmail.com>
2026-09-17 08:02:17 -0300

    Continuação da lógica do olho interativo no arquivo JS.

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,11 @@ document.addEventListener("DOMContentLoaded", function(){
     const campoSenha = document.getElementById("senha");
     const iconeSenha = document.getElementById("iconeSenha");
 
-    document.addEventListener("click", function(){
-        console.log("Clicou no botão!");
+    botaoMostrarSenha.addEventListener("click", function(){
+        if(campoSenha.type == "password"){
+            campoSenha.type = "text";
+            iconeSenha.classList.remove("bi-eye");
+            iconeSenha.classList.add("bi")
+        }
     });
 }); // Para garantir que o HTML exista antes do JS rodar.
